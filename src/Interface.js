bb2d919304bf3f6b4e8980f48a739a0b53d2e80b
Graeme Stirling <graemejstirling@gmail.com>
2021-09-07 17:47:44 +0100

    Developed interface to use notebook class

--- src/Interface.js
+++ src/Interface.js
@@ -1,11 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   const notebook = new Notebook();
+  updateNotes = () => {
+    document.querySelector("#notesList").innerText = notebook.getNotes();
+    // at the moment objects are being returned 
+    // we need strings seperated by new lines 
+    // back-end ???
+    console.log(notebook.getNotes());
+  }
+  updateNotes();
   document.querySelector("#addNote").addEventListener("click", () => {
     const newNote = document.getElementById("notepad").value;
     const note = new Note();
-    note.addText(newNote)
-    notebook.addNote(note)
+    // could notes be intialized with text 
+    note.addText(newNote);
+    notebook.addNote(note);
     document.getElementById("notepad").value = "";
+  updateNotes();
   });
 });
 
