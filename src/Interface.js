4b61ad2dbe863d452787dde3e78fe29bb4c6f105
Graeme Stirling <graemejstirling@gmail.com>
2021-09-08 16:09:58 +0100

    Changes suggested for interface

--- src/Interface.js
+++ src/Interface.js
@@ -2,16 +2,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const notebook = new Notebook();
   updateNotes = () => {
     document.querySelector("#notesList").innerText = notebook.getNotes();
+    // emojify 
+    // truncate 
+    // display note on page
     // at the moment objects are being returned 
-    // we need strings seperated by new lines 
-    // back-end ???
     console.log(notebook.getNotes());
   }
   updateNotes();
   document.querySelector("#addNote").addEventListener("click", () => {
     const newNote = document.getElementById("notepad").value;
     const note = new Note();
-    // could notes be intialized with text 
     note.addText(newNote);
     notebook.addNote(note);
     document.getElementById("notepad").value = "";
