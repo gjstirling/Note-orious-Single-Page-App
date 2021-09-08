c48435a0d5f84cf20700504d6de3ce59709a66df
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-09-08 18:22:14 +0100

    Interface WIP

--- src/Interface.js
+++ src/Interface.js
@@ -1,16 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const notebook = new Notebook();
+
   const updateNotes = () => {
     // get notes text in reverse
     // for each note, emojify and truncate
     // for each note, display as list item with id]
-
-    const notes = notebook.getNotes().reverse()
+    document.getElementById("notesList").innerHTML = ""
     let li;
+    let notes = notebook.getNotes().reverse()
     notes.forEach((element) => {
-      li = document.createElement('li')
-      li.innerHTML = element.getText();
-      document.getElementById("notesList").appendChild(li)
+      emojify(element.getText())
     });
   };
 
@@ -18,27 +17,30 @@ document.addEventListener("DOMContentLoaded", () => {
     const newNote = document.getElementById("notepad").value;
     const note = new Note();
     note.addText(newNote);
+    notebook.getNotes().reverse()
     notebook.addNote(note);
     document.getElementById("notepad").value = "";
     updateNotes();
   });
+
+  function emojify(text) {
+    fetch("https://makers-emojify.herokuapp.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text: text }),
+    })
+      .then((response) => response.json())
+      .then((text) => {
+        li = document.createElement('li')
+        li.innerHTML = text.emojified_text;
+        document.getElementById("notesList").appendChild(li)
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 });
 
-function emojify(text) {
-  fetch("https://makers-emojify.herokuapp.com", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ text: text }),
-  })
-    .then((response) => response.json())
-    .then((text) => {
-      let li = document.createElement('li')
-      li.innerHTML = text.emojified_text
-      document.querySelector('ul').appendChild(li)
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-}
+
