9abc62aaf8904f6d96dd6300516ba4a5ad980d19
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-09-09 14:59:28 +0100

    Interface WIP

--- src/Interface.js
+++ src/Interface.js
@@ -5,25 +5,39 @@ document.addEventListener("DOMContentLoaded", () => {
     // get notes text in reverse
     // for each note, emojify and truncate
     // for each note, display as list item with id]
-    document.getElementById("notesList").innerHTML = ""
-    let li;
-    let notes = notebook.getNotes().reverse()
-    notes.forEach((element) => {
-      emojify(element.getText())
+    // document.getElementById("notesList").innerHTML = ""
+      console.log(document.getElementById("notesList").children)
+    while (document.getElementById("notesList").firstChild) {
+      document.getElementById("notesList").removeChild(document.getElementById("notesList").firstChild);
+  }
+  console.log(document.getElementById("notesList").children)
+    notebook.getNotes().forEach((element, index) => {
+      let li;
+      emojify(element.getText(), index)
     });
+    // const ascending = (a,b) => a.id - b.id
+    // document.getElementById("notesList").sort(ascending)
+    // console.log(notebook.getNotes())
+    // console.log(document.getElementById("notesList").children)
+    // let sorted;
+    // sorted = Array.from(document.getElementById("notesList").children).sort((a, b) => { return b.getAttribute('id') - a.getAttribute('id') })
+    // document.getElementById("notesList").innerHTML = ""
+    // console.log(document.getElementById("notesList").children)
+    // sorted.forEach(e => document.getElementById("notesList").appendChild(e))
+    // console.log(document.getElementById("notesList").children)
   };
 
   document.querySelector("#addNote").addEventListener("click", () => {
     const newNote = document.getElementById("notepad").value;
     const note = new Note();
     note.addText(newNote);
-    notebook.getNotes().reverse()
+
     notebook.addNote(note);
     document.getElementById("notepad").value = "";
     updateNotes();
   });
 
-  function emojify(text) {
+  function emojify(text, index) {
     fetch("https://makers-emojify.herokuapp.com", {
       method: "POST",
       headers: {
@@ -34,6 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
       .then((response) => response.json())
       .then((text) => {
         li = document.createElement('li')
+        li.setAttribute('id', index)
         li.innerHTML = text.emojified_text;
         document.getElementById("notesList").appendChild(li)
       })
