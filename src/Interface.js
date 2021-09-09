c7a9b3d671678bc910f6d44701653f52fbd98c5b
edpackard <edpackard81@gmail.com>
2021-09-09 14:25:02 +0100

    experiments

--- src/Interface.js
+++ src/Interface.js
@@ -1,22 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const notebook = new Notebook();
   updateNotes = () => {
-    document.querySelector("#notesList").innerText = notebook.getNotes();
-    // emojify 
-    // truncate 
-    // display note on page
-    // at the moment objects are being returned 
     console.log(notebook.getNotes());
+    const testing = JSON.stringify(notebook.getNotes()); // testing = the current notebook in a JSON string form
+    console.log("testing:", testing);
+    window.localStorage.setItem("test", testing); // sets localstorage key test to the current notebook in JSON form
+    document.querySelector("#notesList").innerText = notebook.getNotes();
+    // emojify
+    // truncate
+    // display note on page
+    // at the moment objects are being returned
+  };
+
+  const notebook = new Notebook();
+
+  const storage = localStorage.getItem("test");
+  if (storage) {
+    // if there is stuff in local storage
+    if (storage.length) {
+      //console.log("here");
+      const note = new Note(); // create a new note object
+      const testText = localStorage.getItem("test");
+      //console.log(JSON.parse(testText));
+      note.addText(testText); // add contents of local storage test key to note
+      notebook.addNote(note);
+      updateNotes();
+    }
   }
-  updateNotes();
+
   document.querySelector("#addNote").addEventListener("click", () => {
     const newNote = document.getElementById("notepad").value;
     const note = new Note();
     note.addText(newNote);
     notebook.addNote(note);
     document.getElementById("notepad").value = "";
-  updateNotes();
+    updateNotes();
   });
 });
-
-
