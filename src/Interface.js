9d276f2cbc2f223d02d15ee8374fed8176baf436
edpackard <edpackard81@gmail.com>
2021-09-09 15:08:23 +0100

    work in progress on local storage - trying again

--- src/Interface.js
+++ src/Interface.js
@@ -5,6 +5,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const notebook = new Notebook();
 
+  // on page load check if anything in storage?
+  // if so: give it to page - process storage string and give it to updateNotes()
+
+  // processing the storage string - if it is a notebook string?
+  // iterate over each hash in the string - extract the text, id whatever
+  // push each 'element' using note.addText(newNote) --> notebook.addNote
+
+  // if not ?
+
   document.querySelector("#addNote").addEventListener("click", () => {
     const newNote = document.getElementById("notepad").value;
     const note = new Note();
